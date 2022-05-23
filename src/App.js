52a3605639c8607ac44009958a9cf1eb2aede828
aguilanbon <aguilanbon@gmail.com>
2022-05-23 14:31:12 +0800

    Quiz cards styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Card from './components/Card'
 
 function App() {
   return (
     <div className="App">
-      <h1>Quiz app</h1>
+      {/* <h1>Quiz App</h1> */}
+      <Card />
     </div>
   );
 }
